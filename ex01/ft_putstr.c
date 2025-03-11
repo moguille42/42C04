e7c39159c203c712c58c9fabe873be5cc0719922
moguille <mp.guillen27@gmail.com>
2025-03-11 23:03:13 +0100

    c04

--- ex01/ft_putstr.c
+++ ex01/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: moguille <moguille@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 18:53:22 by moguille          #+#    #+#             */
-/*   Updated: 2025/03/10 18:59:02 by moguille         ###   ########.fr       */
+/*   Updated: 2025/03/11 22:09:13 by moguille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,12 @@ void	ft_putstr(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		write
-	
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+/*int	main(void)
+{
+	ft_putstr("hola mundo\n");
+}*/
